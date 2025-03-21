0c357ae163ba4fee7e18fa7975e2497531a53c72
kozmaoliver <kozma.oliver@gmail.com>
2025-03-21 21:14:30 +0100

    feat: Update DocumentationController

--- src/Controller/Portal/DocumentationController.php
+++ src/Controller/Portal/DocumentationController.php
@@ -5,15 +5,25 @@ declare(strict_types=1);
 namespace App\Controller\Portal;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/doc', name: 'doc')]
 class DocumentationController extends AbstractController
 {
+    public function __construct(
+        #[Autowire("%app.host%")]
+        private readonly string $host
+    )
+    {
+    }
+
     #[Route('/', name: '_index', methods: ['GET'])]
     public function indexAction(): Response
     {
-        return $this->render('documentation.html.twig', []);
+        return $this->render('documentation.html.twig', [
+            'apiHost' => $this->host . '/api',
+        ]);
     }
 }
