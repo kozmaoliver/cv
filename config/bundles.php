cee0e6b1cad77628e0e86ac4cc026c702557ee15
kozmaoliver <kozma.oliver@gmail.com>
2025-04-04 11:56:09 +0200

    feat: Add security and 2fa bundle

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
 ];
